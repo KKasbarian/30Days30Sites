9e2de92c2489a86e53bb2ec7900f65bba35d1d91
Kourken Kasbarian <kourkenk@outlook.com>
2017-08-04 22:48:25 +0400

    Day 10 Project Added

---
+++ Kids-Toy/js/script.js
@@ -0,0 +1,18 @@
+/* Scroll to top function */
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 400 || document.documentElement.scrollTop > 300) {
+        document.getElementById("myBtn").style.display = "block";
+    } else {
+        document.getElementById("myBtn").style.display = "none";
+    }
+}
+
+function topFunction() {
+    document.body.scrollTop = 0; //
+    document.documentElement.scrollTop = 0; //
+}
+
+/* End Scroll to top function */
